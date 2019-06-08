956a535595d740be89ea8e8a02d38ddfbc3947fe
Flávio Tassan <tassanf@gmail.com>
2019-06-08 18:34:56 -0300

    First commit

---
+++ src/components/Pokedex.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+import Pokemon from './Pokemon';
+import List from '@material-ui/core/List'
+import { Grid } from '@material-ui/core';
+
+class Pokedex extends Component {
+    static displayName = Pokedex.name;
+
+    constructor (props) {
+        super(props);
+        this.state = { pokemons: [], loading: true };
+       // img src = //cdn.bulbagarden.net/upload/d/d4/770MS.png
+        fetch('https://pokeapi.co/api/v2/pokemon/?limit=151')
+            .then(response => response.json())
+            .then(data => {
+                this.setState({ pokemons: data, loading: false });
+            });
+    }
+
+    static renderPokedex (pokemons) {
+        return (
+          <Grid container spacing={0}>
+              {pokemons.results.map(pkm => 
+                <Grid item xs={3} >
+                  <Pokemon pokemonName={pkm.name}></Pokemon>
+                </Grid>
+                )}
+          </Grid>
+          
+        );
+      }
+    
+      render () {
+        let contents = this.state.loading
+          ? <p><em>Loading...</em></p>
+          : Pokedex.renderPokedex(this.state.pokemons);
+    
+        return (
+          <div>
+            <h1>Pokedex</h1>
+            <p>Pokémon's Pokedex built with React, PokéAPI and PokemonDB</p>
+            {contents}
+          </div>
+        );
+      }
+}
+
+export default Pokedex;
---
+++ src/components/Pokemon.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import ListItem from '@material-ui/core/ListItem'
+import { ListItemText, ListItemAvatar, Avatar, Paper, Image } from '@material-ui/core';
+import './Pokemon.css';
+
+export class Pokemon extends Component {
+  static displayName = Pokemon.name;
+
+  constructor (props) {
+    super(props);
+    this.state = { pokemon: [], loading: true };
+
+    fetch('https://pokeapi.co/api/v2/pokemon/'+ props.pokemonName) // index
+      .then(response => response.json())
+      .then(data => {
+        this.setState({ pokemon: data, loading: false });
+      });
+  }
+
+  //   cdn.bulbagarden.net/upload/d/d4/{pokemon.id}.png
+  // https://assets.pokemon.com/assets/cms2/img/pokedex/full/809.png
+  static renderPokemon (pokemon) {
+    var url = "https://cdn.bulbagarden.net/upload/e/ec/"
+    var extension = "MS.png";
+    var str = "" + pokemon.id
+    var pad = "000"
+    var ans = pad.substring(0, pad.length - str.length) + str
+    var link = url + ans + extension;
+
+    return (
+      <Paper className="spacing">
+        <ListItem title={pokemon.name}>
+          <ListItemAvatar>
+            <Avatar alt={pokemon.name} src={`https://img.pokemondb.net/sprites/sun-moon/icon/${pokemon.name}.png`} />
+          </ListItemAvatar>
+          <ListItemText>
+            {pokemon.name}
+          </ListItemText>
+        </ListItem>
+      </Paper>
+    );
+  }
+
+  render () {
+    let contents = this.state.loading
+      ? <p><em>Loading...</em></p>
+      : Pokemon.renderPokemon(this.state.pokemon);
+
+    return (
+      <div>
+        {contents}
+      </div>
+    );
+  }
+}
+
+export default Pokemon;
